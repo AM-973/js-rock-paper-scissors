c2deaa5ae37a089780e6f550b18e7cb7a61490c3
“AM-973” <AlMatar973@gmail.com>
2025-07-16 23:30:00 +0300

    made code more efficient

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 /*------------ Constants ------------------*/
-const choice = ['rock', 'paper', 'scissors']
-
-
+const choices = ['rock', 'paper', 'scissors']
+const resultDisplayEl = document.querySelector('#result-display')
+const resetBtnEl = document.querySelector('#resetButton')
+const allBtns = document.querySelectorAll('.game-button')
+console.log(allBtns)
 /*------------- Variables -----------------*/
 let msg
 let playerChoice
@@ -9,70 +11,51 @@ let computerChoice
 
 
 /*------ Cached Element References ---------*/
-const rockBtnEl = document.querySelector('#rock')
-const paperBtnEl = document.querySelector('#paper')
-const scissorsBtnEl = document.querySelector('#scissors')
-const resultDisplayEl = document.querySelector('#result-display')
-const resetBtnEl = document.querySelector('#resetButton')
-
-
+allBtns.forEach((btn)=>{
+    btn.addEventListener('click', play)
+})
 /*-------- Functions -----------------------*/
+
 function getComputerChoice() {
-    // generate a random number 0-2
-    const randomIndex = Math.floor(Math.random() * choice.length)
-    // select the item from the array
-    return choice[randomIndex]
+    const randomIndex = Math.floor(Math.random() * choices.length)
+    return choices[randomIndex]
 }
 
-// initialize game state
 function play() {
     computerChoice = getComputerChoice()
     playerChoice = event.target.id
     compare()
-    // after updating state, render to html
     render()
 }
 
 
-// updates our UI/html directly
-function render() {
-    resultDisplayEl.textContent = `Computer chose ${computerChoice} 
-    and you chose ${playerChoice}....
-    ${msg}`
-}
-
-function compare(){
-   if (playerChoice === computerChoice) {
+function compare() {
+    if (playerChoice === computerChoice) {
         msg = "It's a tie!";
     }
-    else if (playerChoice === choice[0] && computerChoice === choice[2]) {
+    else if (playerChoice === choices[0] && computerChoice === choices[2]) {
         msg = "You win!";
     }
-    else if (playerChoice === choice[1] && computerChoice === choice[0]) {
+    else if (playerChoice === choices[1] && computerChoice === choices[0]) {
         msg = "You win!";
     }
-    else if (playerChoice === choice[2] && computerChoice === choice[1]) {
+    else if (playerChoice === choices[2] && computerChoice === choices[1]) {
         msg = "You win!";
     }
-    else { 
+    else {
         msg = "You lost.";
     }
 }
 
-
-function test() {
-    console.log('you clicked me')
+const render = () => {
+  resultDisplayEl.textContent = `You chose ${playerChoice} and the computer chose ${computerChoice}. ${msg}`
 }
 
-function resetGame(){
+function resetGame() {
     resultDisplayEl.textContent = ''
     playerChoice = null
     computerChoice = null
 
 }
-
 /*--------- Event Listeners ----------------*/
-rockBtnEl.addEventListener('click', play)
-paperBtnEl.addEventListener('click', play)
-scissorsBtnEl.addEventListener('click', play)
 resetBtnEl.addEventListener('click', resetGame)
